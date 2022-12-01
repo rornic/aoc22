0271caefcc3723d8b2e909f51ec6b368a70e4277
Rory N <rorynickolls@gmail.com>
2022-12-01 08:40:40 +0000

    Solve day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,7 +3,7 @@ use aoc22::input::read_input;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_input("day1")?;
 
-    let elves: Vec<u32> = input.lines().fold(vec![0], |elves, line| {
+    let mut elves: Vec<u32> = input.lines().fold(vec![0], |elves, line| {
         let mut new_elves = elves.clone();
         if line.is_empty() {
             new_elves.push(0);
@@ -14,7 +14,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         new_elves
     });
 
-    println!("total calories {}", elves.iter().max().unwrap());
+    println!(
+        "max calories for single elf {}",
+        elves.iter().max().unwrap()
+    );
+
+    elves.sort();
+    elves.reverse();
+    println!(
+        "total calories for max three elves {}",
+        [elves[0], elves[1], elves[2]].iter().sum::<u32>()
+    );
 
     Ok(())
 }
