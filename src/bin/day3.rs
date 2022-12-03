2ab056daa7c6fdf1574bd47024b1269d11ec20b6
Rory N <rorynickolls@gmail.com>
2022-12-03 09:35:17 +0000

    Solve day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -5,12 +5,11 @@ use aoc22::input::read_input;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_input("day3")?;
 
-    let total_sum: u64 = input
+    let sum: u64 = input
         .lines()
         .into_iter()
         .map(|line| {
-            let c1: HashSet<char> = line.chars().take(line.len() / 2).collect();
-            let c2: HashSet<char> = line.chars().skip(line.len() / 2).collect();
+            let (c1, c2) = rucksack_to_compartments(line);
             let intersect: HashSet<&char> = c1.intersection(&c2).collect();
             intersect
                 .into_iter()
@@ -18,9 +17,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .sum::<u64>()
         })
         .sum();
-    println!("total common item sum {}", total_sum);
+    println!("total common item sum {}", sum);
+
+    let badge_sum = &input
+        .lines()
+        .map(|line| line.chars().collect::<HashSet<char>>())
+        .collect::<Vec<HashSet<char>>>()
+        .chunks(3)
+        .fold(0, |acc, elves| {
+            let intersect: HashSet<char> = elves
+                .into_iter()
+                .cloned()
+                .fold(elves.get(0).unwrap().clone(), |s1, s2| {
+                    s1.intersection(&s2).cloned().collect::<HashSet<char>>()
+                });
+            let badge = intersect.iter().next().unwrap();
+            acc + item_to_priority(*badge)
+        });
+    println!("total badge sum {}", badge_sum);
 
     Ok(())
+}
+
+fn rucksack_to_compartments(rucksack: &str) -> (HashSet<char>, HashSet<char>) {
+    let c1: HashSet<char> = rucksack.chars().take(rucksack.len() / 2).collect();
+    let c2: HashSet<char> = rucksack.chars().skip(rucksack.len() / 2).collect();
+    return (c1, c2);
 }
 
 fn item_to_priority(item: char) -> u64 {
