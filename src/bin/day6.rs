c1d3cf34fa0f44d9383d1f8ae1ff6c9d6af6aff5
Rory N <rorynickolls@gmail.com>
2022-12-06 07:22:52 +0000

    Solve day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -4,19 +4,27 @@ use aoc22::input::read_input;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_input("day6")?;
-
     let chars = input.chars().collect::<Vec<char>>();
-    let windows = chars.windows(4).collect::<Vec<&[char]>>();
+
+    let start_of_packet_index = first_distinct_window(&chars, 4);
+    println!("start of packet index {}", start_of_packet_index);
+
+    let start_of_message_index = first_distinct_window(&chars, 14);
+    println!("start of message index {}", start_of_message_index);
+    Ok(())
+}
+
+fn first_distinct_window(chars: &Vec<char>, n: usize) -> usize {
+    let windows = chars.windows(n).collect::<Vec<&[char]>>();
 
     let mut start_index = 0;
     for (i, window) in windows.iter().cloned().enumerate() {
         let set = window.iter().cloned().collect::<HashSet<char>>();
-        if set.len() == 4 {
-            start_index = i + 4;
+        if set.len() == n {
+            start_index = i + n;
             break;
         }
     }
 
-    println!("start of packet index {}", start_index);
-    Ok(())
+    start_index
 }
