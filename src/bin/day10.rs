18bdf09cd2805223a1ccf826cca084f4defe2754
Rory N <rorynickolls@gmail.com>
2022-12-10 09:40:12 +0000

    Solve day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -4,11 +4,83 @@ enum Instruction {
     AddX(i32),
     NoOp,
 }
+struct Cpu {
+    x: i32,
+    // Register used as flag for executing addx over multiple cycles
+    y: i32,
+    // Program counter keeps track of where we are in execution
+    pc: usize,
+}
+
+impl Cpu {
+    fn new() -> Cpu {
+        Cpu { x: 1, y: 0, pc: 0 }
+    }
+
+    fn run(&mut self, instructions: &Vec<Instruction>, mut execute_cycle: impl FnMut(&mut Cpu)) {
+        while self.pc < instructions.len() {
+            execute_cycle(self);
+
+            let instruction = &instructions[self.pc];
+            match instruction {
+                Instruction::AddX(val) => {
+                    if self.y == 1 {
+                        self.x += val;
+                        self.y = 0;
+                        self.pc += 1;
+                    } else {
+                        self.y = 1;
+                    }
+                }
+                Instruction::NoOp => self.pc += 1,
+            }
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = read_input("day10")?;
+    let instructions = read_instructions(input);
 
-    let instructions: Vec<Instruction> = input
+    // Part 1
+    let mut cpu1 = Cpu::new();
+    let mut signal_strength = 0;
+    let mut cycles = 0;
+    cpu1.run(&instructions, |cpu| {
+        cycles += 1;
+        if cycles == 20
+            || cycles == 60
+            || cycles == 100
+            || cycles == 140
+            || cycles == 180
+            || cycles == 220
+        {
+            signal_strength += cycles * cpu.x;
+        }
+    });
+    println!("signal strength is {}", signal_strength);
+
+    // Part 2
+    let mut cpu2 = Cpu::new();
+    let mut col = 0;
+    cpu2.run(&instructions, |cpu| {
+        if cpu.x.abs_diff(col) <= 1 {
+            print!("{}", '#');
+        } else {
+            print!("{}", '.');
+        }
+        col += 1;
+        if col >= 40 {
+            print!("\n");
+            col = 0;
+        }
+    });
+
+    Ok(())
+}
+
+fn read_instructions(input: String) -> Vec<Instruction> {
+    input
         .lines()
         .map(|l| {
             let parts: Vec<&str> = l.split(" ").collect();
@@ -18,48 +90,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 _ => panic!("unknown instruction {}", parts[0]),
             }
         })
-        .collect();
-
-    let mut signal_strength = 0;
-
-    // x register stores the signal strength value
-    let mut x = 1;
-
-    // y register is used as a flag for when we are executing an addx instruction
-    let mut y = 0;
-
-    let mut cycles = 1;
-    let mut program_counter = 0;
-    while program_counter < instructions.len() {
-        // Refactor this in part 2
-        if cycles == 20
-            || cycles == 60
-            || cycles == 100
-            || cycles == 140
-            || cycles == 180
-            || cycles == 220
-        {
-            signal_strength += cycles * x;
-        }
-
-        let instruction = &instructions[program_counter];
-        match instruction {
-            Instruction::AddX(val) => {
-                if y == 1 {
-                    x += val;
-                    y = 0;
-                    program_counter += 1;
-                } else {
-                    y = 1;
-                }
-            }
-            Instruction::NoOp => program_counter += 1,
-        }
-        cycles += 1;
-        println!("cycle {}: {}", cycles, x);
-    }
-
-    println!("signal strength is {}", signal_strength);
-
-    Ok(())
+        .collect()
 }
